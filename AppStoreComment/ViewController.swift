0fac98f72ddb34994380db3bf7d233271e1f581d
Feng <fy@FydeMacBook-Pro.lan>
2017-04-19 10:55:49 +0800

    add appIcon

--- AppStoreComment/ViewController.swift
+++ AppStoreComment/ViewController.swift
@@ -7,12 +7,28 @@
 //
 
 import UIKit
+import StoreKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let button = UIButton(type: .custom)
+        button.addTarget(self, action: #selector(pushAppStoreComment), for: .touchUpInside)
+        button.frame = CGRect(x: 100, y: 100, width: 100, height: 50)
+        button.setTitle("评论", for: .normal)
+        button.setTitleColor(UIColor.black, for: .normal)
+        view.addSubview(button)
+        
+        let button1 = UIButton(type: .custom)
+        button1.addTarget(self, action: #selector(alternateIcon), for: .touchUpInside)
+        button1.frame = CGRect(x: 100, y: 200, width: 100, height: 50)
+        button1.setTitle("更换icon", for: .normal)
+        button1.setTitleColor(UIColor.black, for: .normal)
+        view.addSubview(button1)
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +36,35 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        pushAppStoreComment()
+    }
 
+    func pushAppStoreComment() {
+        
+        SKStoreReviewController.requestReview()
+    }
+    
+    func alternateIcon() {
+        if UIApplication.shared.supportsAlternateIcons {
+            print("you can change this app's icon")
+        }else {
+            print("you cannot change this app's icon")
+            return
+        }
+        
+        if let name = UIApplication.shared.alternateIconName {
+            // CHANGE TO PRIMARY ICON
+            UIApplication.shared.setAlternateIconName(nil) { (err:Error?) in
+                print("set icon error：\(String(describing: err))")
+            }
+            print("the alternate icon's name is \(name)")
+        }else {
+            // CHANGE TO ALTERNATE ICON
+            UIApplication.shared.setAlternateIconName("blackBgColor") { (err:Error?) in
+                print("set icon error：\(String(describing: err))")
+            }
+        }
+    }
 }
 
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 #  AppStoreComment
+
+*** iOS 10.3+ ***
+
+
+
+## 新增app用户评论
+
+## 新增appIcon更换
+   plist文件修改配置：Icon files (iOS 5)
+   
